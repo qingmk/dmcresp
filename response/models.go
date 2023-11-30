15f647972ae12d68f23b8aec84f7dfff7ba248b3
qinghua <15070102782@139.com>
2023-11-30 13:02:16 +0800

    modify

--- response/models.go
+++ response/models.go
@@ -1,7 +1,7 @@
 package response
 
 type RespData interface{}
-type RespDataJson interface{}
+type RespDataJson map[string]interface{}
 type AckBody struct {
 	Code int      `json:"code"`
 	Msg  string   `json:"msg"`
